0e35adffe8cbe074632706908ae6958ae329c2a2
michaelfong2017 <michaelfong2017@gmail.com>
2023-09-25 00:41:13 +0800

    glWidget event loop, PySide2 keyPressEvent

--- mainwindow.py
+++ mainwindow.py
@@ -1,13 +1,20 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2.QtCore import QFile
+from PySide2 import QtCore
 from ui_mainwindow import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.glWidget = self.ui.openGLWidget
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.glWidget.updateGL)
+        timer.start(40)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
