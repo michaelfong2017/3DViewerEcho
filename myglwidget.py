4ce37b5721bc37204b10a6e481f1638b2d5707ac
michaelfong2017 <michaelfong2017@gmail.com>
2023-09-25 01:16:55 +0800

    Render a triangle

--- myglwidget.py
+++ myglwidget.py
@@ -1,6 +1,7 @@
 import sys
 from PySide2 import QtCore, QtOpenGL
 import moderngl as mgl
+from model import Triangle
 
 
 class MyGLWidget(QtOpenGL.QGLWidget):
@@ -17,23 +18,31 @@ class MyGLWidget(QtOpenGL.QGLWidget):
     def keyPressEvent(self, e):
         print(e.key())
         if e.key() == QtCore.Qt.Key_Q:
+            self.scene.destroy()
             sys.exit()
         else:
             super().keyPressEvent(e)
 
     def mousePressEvent(self, e):
         super().mousePressEvent(e)
+        print(f'QGLWidget coordinates: ({e.x()}, {e.y()})')
         if e.button() == QtCore.Qt.LeftButton:
-            print("press")
+            print("LeftButton is pressed")
 
     def initializeGL(self):
         self.ctx = mgl.create_context()
 
+        self.scene = Triangle(self)
+
     def paintGL(self):
         self.ctx.clear(color=(0.08, 0.16, 0.18))
 
+        self.scene.render()
+
     def resizeGL(self, width, height):
-        pass
+        width = max(2, width)
+        height = max(2, height)
+        self.ctx.viewport = (0, 0, width, height)
 
     # def free_resources(self):
     #     """Helper to clean up resources."""
