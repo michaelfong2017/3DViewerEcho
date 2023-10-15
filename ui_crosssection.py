fbcc6e07430f88ab4961f824e1915044e00bb82f
Michael Fong <michaelfong2017@gmail.com>
2023-10-16 04:17:01 +0800

    Fix image display to correct aspect ratio (max height 240px)

--- ui_crosssection.py
+++ ui_crosssection.py
@@ -18,7 +18,7 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(242, 300)
+        Form.resize(422, 300)
         self.verticalLayout = QVBoxLayout(Form)
         self.verticalLayout.setSpacing(6)
         self.verticalLayout.setObjectName(u"verticalLayout")
@@ -27,25 +27,28 @@ class Ui_Form(object):
         self.verticalLayout_10.setObjectName(u"verticalLayout_10")
         self.pushButton_13 = QPushButton(Form)
         self.pushButton_13.setObjectName(u"pushButton_13")
-        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButton_13.sizePolicy().hasHeightForWidth())
         self.pushButton_13.setSizePolicy(sizePolicy)
-        self.pushButton_13.setMinimumSize(QSize(240, 0))
-        self.pushButton_13.setMaximumSize(QSize(240, 16777215))
+        self.pushButton_13.setMinimumSize(QSize(420, 0))
+        self.pushButton_13.setMaximumSize(QSize(16777215, 16777215))
         self.pushButton_13.setStyleSheet(u"font: 10px \"MS Shell Dlg 2\";")
 
         self.verticalLayout_10.addWidget(self.pushButton_13)
 
         self.label_8 = QLabel(Form)
         self.label_8.setObjectName(u"label_8")
-        sizePolicy.setHeightForWidth(self.label_8.sizePolicy().hasHeightForWidth())
-        self.label_8.setSizePolicy(sizePolicy)
-        self.label_8.setMinimumSize(QSize(240, 240))
-        self.label_8.setMaximumSize(QSize(240, 240))
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label_8.sizePolicy().hasHeightForWidth())
+        self.label_8.setSizePolicy(sizePolicy1)
+        self.label_8.setMinimumSize(QSize(0, 240))
+        self.label_8.setMaximumSize(QSize(16777215, 240))
         self.label_8.setPixmap(QPixmap(u":/images/annotated_heart_sample.png"))
-        self.label_8.setScaledContents(True)
+        self.label_8.setScaledContents(False)
 
         self.verticalLayout_10.addWidget(self.label_8)
 
@@ -53,8 +56,8 @@ class Ui_Form(object):
         self.pushButton_9.setObjectName(u"pushButton_9")
         sizePolicy.setHeightForWidth(self.pushButton_9.sizePolicy().hasHeightForWidth())
         self.pushButton_9.setSizePolicy(sizePolicy)
-        self.pushButton_9.setMinimumSize(QSize(240, 0))
-        self.pushButton_9.setMaximumSize(QSize(240, 16777215))
+        self.pushButton_9.setMinimumSize(QSize(420, 0))
+        self.pushButton_9.setMaximumSize(QSize(16777215, 16777215))
 
         self.verticalLayout_10.addWidget(self.pushButton_9)
 
