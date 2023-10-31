a8c68725e04a14fd4d40e474dfad9e501366f65e
Michael Fong <michaelfong2017@gmail.com>
2023-10-31 14:18:45 +0800

    Click to view the cross section in full screen

--- clickableqlabel.py
+++ clickableqlabel.py
@@ -1,5 +1,5 @@
 from PySide2 import QtCore
-from PySide2.QtWidgets import QLabel
+from PySide2.QtWidgets import QApplication, QLabel, QDialog, QVBoxLayout
 
 
 class ClickableQLabel(QLabel):
@@ -10,3 +10,29 @@ class ClickableQLabel(QLabel):
         if event.buttons() == QtCore.Qt.LeftButton:
             width = self.pixmap().width()
             print("Pixmap width:", width)
+            dialog = FullScreenDialog(self.pixmap())
+            dialog.exec_()
+
+
+class FullScreenDialog(QDialog):
+    def __init__(self, pixmap):
+        super().__init__()
+        self.setWindowTitle("Cross Section")
+        self.setWindowState(self.windowState() | QtCore.Qt.WindowFullScreen)
+
+        self.label = QLabel()
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        scaled_pixmap = pixmap.scaledToHeight(QApplication.desktop().screenGeometry().height() * 3.0 / 5.0, aspectRatioMode=QtCore.Qt.KeepAspectRatio)
+        self.label.setPixmap(scaled_pixmap)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+        # print(f"scaled_pixmap: {scaled_pixmap.width()}, {scaled_pixmap.height()}")
+        # print(f"self.label: {self.label.width()}, {self.label.height()}")
+        # print(f"self: {self.width()}, {self.height()}")
+        # print(f"layout: {layout.width()}, {layout.height()}")
+
+    def mousePressEvent(self, event):
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.close()
